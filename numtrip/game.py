cf1eec47d31522e561dbdc9156cd630df2ba665e
TJ001 <tj5036a@gmail.com>
2022-10-01 22:40:45 +0200

    Fixed print_board()

--- numtrip/game.py
+++ numtrip/game.py
@@ -7,14 +7,16 @@ colorama.init()
 
 colors = [Back.GREEN, Back.BLUE, Back.GREEN, Back.RED, Back.YELLOW, Back.CYAN, Back.WHITE, Back.MAGENTA]
 
-# Create a random gameboard
-def randomboard(size: int):
-    return [
-        [2**random.randint(2, i+j) for i in range(1, size+1)] for j in range(1, size+1)
-    ] 
 
-# Print a pretty board
-def print_board(board: list):
+def randomboard(size: int):
+    """Generate a random board"""
+    return [
+        [2**random.randint(2, i + j) for i in range(1, size + 1)] for j in range(1, size + 1)
+    ]
+
+
+def print_board(board):
+    """Print a pretty board"""
     max_len = 1
     size = len(board)
 
@@ -27,34 +29,30 @@ def print_board(board: list):
     # Print board
     for line in board:
         for i in line:
-            print("+-----", end='')
+            print("+-------", end='')
         print("+")
         for i in line:
-            print("|     ", end='')
+            print("|       ", end='')
         print("|")
         for i in line:
-            spaces = 5-len(str(i))
-            spaces_l = math.floor(spaces/2)
-            spaces_r = math.ceil(spaces/2)
-            for i in range(spaces_l):
+            print("|", end='')
+            spaces = 7 - len(str(i))
+            spaces_l = math.floor(spaces / 2)
+            spaces_r = math.ceil(spaces / 2)
+            for l in range(spaces_l):
                 print(" ", end='')
-            print(str(i), end='')
-            for i in range(spaces_r):
+            print(i, end='')
+            for l in range(spaces_r):
                 print(" ", end='')
-        print(" ")
+        print("|")
+        for i in line:
+            print("|       ", end='')
+        print("|")
+    for i in line:
+        print("+-------", end='')
+    print("+")
 
-        
+
 board = randomboard(4)
 print(board)
 print_board(board)
-                
-
-
-""" for n in line:
-            spaces = max_len - len(str(n))
-            print(colors[int(math.log2(n))-1] + str(n), end='')
-            print(colorama.Style.RESET_ALL, end='')
-            for i in range(3 + spaces):
-                print(" ", end='')
-        print(" ")
- """
