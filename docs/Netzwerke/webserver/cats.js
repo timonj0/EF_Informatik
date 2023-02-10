c223ac0789959d9fbf31f0687911441a9e8209af
TJ001 <tj5036a@gmail.com>
2023-02-10 16:25:46 +0100

    Add cat.png

---
+++ docs/Netzwerke/webserver/cats.js
@@ -0,0 +1,11 @@
+// Canvas with cats bouncing around
+
+// The canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// The cat image
+var cat = new Image();
+cat.src = "cat.png";
+
+// The cats
